df70fb670cc98d40b1d8d03c9f0403e4096ff590
GomSon-E <gomsonixx@gmail.com>
2023-01-30 21:11:10 +0900

    01/30 : Spring (Util - Transaction, Batch, Query Logging)

---
+++ 21_spring_util/src/main/java/com/spring/util/transaction/TransactionService.java
@@ -0,0 +1,30 @@
+package com.spring.util.transaction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class TransactionService {
+
+	@Autowired
+	private TransactionDao transactionDao;
+	
+	@Transactional
+	public void addAccountTransfer() throws Exception {
+		transactionDao.updateSendMoney();
+		//System.out.println(1/0); 	// 문제 발생
+		transactionDao.updateRecvMoney();
+	}
+	
+	@Transactional
+	public void addOrder() throws Exception {
+		transactionDao.updatePoint();
+		transactionDao.updateCartCnt();
+		//System.out.println(1/0);	// 문제 발생
+		transactionDao.updateOrderCnt();
+	}
+	
+	
+	
+}
